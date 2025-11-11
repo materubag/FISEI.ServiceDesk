85fa91ce048ae0b52b79c50d8782b947b10e1cc3
materubag <mrubio7466@uta.edu.ec>
2025-11-10 22:40:31 -0500

    Agregando migraciones

---
+++ FISEI.ServiceDesk.Infrastructure/Migrations/20251111033800_CoreEntities.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FISEI.ServiceDesk.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CoreEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ComentariosIncidencia",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IncidenciaId = table.Column<long>(type: "bigint", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Texto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EsInterno = table.Column<bool>(type: "bit", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComentariosIncidencia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EstadosIncidencia",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Codigo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    EsFinal = table.Column<bool>(type: "bit", nullable: false),
+                    Orden = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EstadosIncidencia", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedbacks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IncidenciaId = table.Column<long>(type: "bigint", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Puntuacion = table.Column<byte>(type: "tinyint", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedbacks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Incidencias",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreadorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TecnicoAsignadoId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ServicioId = table.Column<int>(type: "int", nullable: false),
+                    PrioridadId = table.Column<int>(type: "int", nullable: false),
+                    EstadoId = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaUltimoCambio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaResolucion = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Cerrada = table.Column<bool>(type: "bit", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Incidencias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notificaciones",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioDestinoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Referencia = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Mensaje = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Leida = table.Column<bool>(type: "bit", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notificaciones", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prioridades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Peso = table.Column<int>(type: "int", nullable: false),
+                    TiempoMaxHoras = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prioridades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seguimientos",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IncidenciaId = table.Column<long>(type: "bigint", nullable: false),
+                    UsuarioId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    EstadoAnteriorId = table.Column<int>(type: "int", nullable: true),
+                    EstadoNuevoId = table.Column<int>(type: "int", nullable: true),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seguimientos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Correo = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RolId = table.Column<int>(type: "int", nullable: false),
+                    Activo = table.Column<bool>(type: "bit", nullable: false),
+                    FechaRegistro = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "Activo", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, true, "Redes" },
+                    { 2, true, "Ofimática" },
+                    { 3, true, "AulaVirtual" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "EstadosIncidencia",
+                columns: new[] { "Id", "Codigo", "EsFinal", "Orden" },
+                values: new object[,]
+                {
+                    { 1, "REPORTADO", false, 1 },
+                    { 2, "ASIGNADO", false, 2 },
+                    { 3, "EN_PROCESO", false, 3 },
+                    { 4, "RESUELTO", false, 4 },
+                    { 5, "CERRADO", true, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Prioridades",
+                columns: new[] { "Id", "Nombre", "Peso", "TiempoMaxHoras" },
+                values: new object[,]
+                {
+                    { 1, "Baja", 1, 72 },
+                    { 2, "Media", 2, 48 },
+                    { 3, "Alta", 3, 24 },
+                    { 4, "Critica", 4, 8 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "Estudiante" },
+                    { 2, "Tecnico" },
+                    { 3, "Administrador" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Servicios",
+                columns: new[] { "Id", "Activo", "CategoriaId", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, true, 1, "WiFi" },
+                    { 2, true, 1, "Ethernet" },
+                    { 3, true, 2, "Correo" },
+                    { 4, true, 2, "Impresoras" },
+                    { 5, true, 3, "AulaVirtual" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Activo", "Correo", "FechaRegistro", "Nombre", "PasswordHash", "RolId" },
+                values: new object[,]
+                {
+                    { new Guid("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaa1"), true, "estudiante@demo.local", new DateTime(2025, 11, 11, 3, 38, 0, 19, DateTimeKind.Utc).AddTicks(7738), "Estudiante Demo", "hash", 1 },
+                    { new Guid("bbbbbbbb-bbbb-bbbb-bbbb-bbbbbbbbbbb2"), true, "tecnico@demo.local", new DateTime(2025, 11, 11, 3, 38, 0, 19, DateTimeKind.Utc).AddTicks(8681), "Tecnico Demo", "hash", 2 },
+                    { new Guid("cccccccc-cccc-cccc-cccc-ccccccccccc3"), true, "admin@demo.local", new DateTime(2025, 11, 11, 3, 38, 0, 19, DateTimeKind.Utc).AddTicks(8684), "Admin Demo", "hash", 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EstadosIncidencia_Codigo",
+                table: "EstadosIncidencia",
+                column: "Codigo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Correo",
+                table: "Usuarios",
+                column: "Correo",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categorias");
+
+            migrationBuilder.DropTable(
+                name: "ComentariosIncidencia");
+
+            migrationBuilder.DropTable(
+                name: "EstadosIncidencia");
+
+            migrationBuilder.DropTable(
+                name: "Feedbacks");
+
+            migrationBuilder.DropTable(
+                name: "Incidencias");
+
+            migrationBuilder.DropTable(
+                name: "Notificaciones");
+
+            migrationBuilder.DropTable(
+                name: "Prioridades");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Seguimientos");
+
+            migrationBuilder.DropTable(
+                name: "Servicios");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
