ca8beec584aec73ce8c12e04e2e46e0b0d9e4e3f
Ismael <Jcuadrado1092@uta.edu.ec>
2025-12-02 21:48:27 -0500

    Se corrije

---
+++ FISEI.ServiceDesk.Web/Pages/Estudiante.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FISEI.ServiceDesk.Web.Pages
+{
+    public class EstudianteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
